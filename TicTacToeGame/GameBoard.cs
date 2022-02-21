3f2fcf4186c9c6056e678a013a6e81c6b2eb10be
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-21 10:08:08 +0530

    Adding Game

--- TicTacToeGame/GameBoard.cs
+++ TicTacToeGame/GameBoard.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TicTacToeGame
 {
-    internal class GameBoard
+    internal class TicTacToe
     {
         
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };

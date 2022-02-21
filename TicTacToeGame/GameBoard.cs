81816214dae113912172ec06711cc5505150ed72
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-21 10:02:07 +0530

    Adding Game

---
+++ TicTacToeGame/GameBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class GameBoard
+    {
+        
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1; 
+        static int choice; 
+        static int flag = 0;
+    }
+}
---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Tic Tac Toe Game");
+            Console.ReadLine();
+
+        }
+    }
+}
